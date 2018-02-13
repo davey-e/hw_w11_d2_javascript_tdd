01abbe46de90d3f15f72f83ac2ba309db4596826
Dave Ellis <dave@ellisnetmail.co.uk>
2018-02-13 21:20:58 +0000

    Added removeDinosaursOfType method to Park with test

--- park.js
+++ park.js
@@ -6,4 +6,10 @@ Park.prototype.addDinosaur = function(dinosaur){
   this.enclosure.push(dinosaur);
 }
 
+Park.prototype.removeDinosaursOfType = function(type){
+  this.enclosure = this.enclosure.filter(function(dinosaur) {
+    return dinosaur.type !== type
+  });
+}
+
 module.exports = Park;
--- specs/park_spec.js
+++ specs/park_spec.js
@@ -6,7 +6,9 @@ describe('Park', function(){
 
   beforeEach(function(){
       park = new Park();
-      triceratops = new Dinosaur('Triceratops', 4);
+      triceratops1 = new Dinosaur('Triceratops', 4);
+      triceratops2 = new Dinosaur('Triceratops', 4);
+      velociraptor = new Dinosaur('Velociraptor', 2);
   });
 
   it('should have an empty enclosure', function(){
@@ -15,9 +17,20 @@ describe('Park', function(){
   });
 
   it('should be able to add a dinosaur to enclosure', function(){
-    park.addDinosaur(triceratops);
+    park.addDinosaur(triceratops1);
     const actual = park.enclosure;
-    assert.deepStrictEqual(actual, [triceratops]);
+    assert.deepStrictEqual(actual, [triceratops1]);
+  });
+
+  it('should be able to remove all dinosaurs of specified type', function(){
+    park.addDinosaur(triceratops1);
+    park.addDinosaur(triceratops2);
+    park.addDinosaur(velociraptor);
+    const actualBefore = park.enclosure;
+    assert.deepStrictEqual(actualBefore, [triceratops1, triceratops2, velociraptor]);
+    park.removeDinosaursOfType('Triceratops');
+    const actualAfter = park.enclosure;
+    assert.deepStrictEqual(actualAfter, [velociraptor]);
   })
 
 });
