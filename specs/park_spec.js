de7c8bd5a98bc6fff732023c2568fc837c514128
Dave Ellis <dave@ellisnetmail.co.uk>
2018-02-13 17:34:23 +0000

    Added addDinosaur method to Park with test

--- specs/park_spec.js
+++ specs/park_spec.js
@@ -6,11 +6,18 @@ describe('Park', function(){
 
   beforeEach(function(){
       park = new Park();
+      triceratops = new Dinosaur('Triceratops', 4);
   });
 
   it('should have an empty enclosure', function(){
     const actual = park.enclosure;
     assert.deepStrictEqual(actual, []);
   });
+
+  it('should be able to add a dinosaur to enclosure', function(){
+    park.addDinosaur(triceratops);
+    const actual = park.enclosure;
+    assert.deepStrictEqual(actual, [triceratops]);
+  })
 
 });
