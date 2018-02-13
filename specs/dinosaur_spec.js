a127031d4bc97bb95e11c7ccb8a94486252f2623
Dave Ellis <dave@ellisnetmail.co.uk>
2018-02-13 17:20:50 +0000

    Added offspringPerYear property to Dinosaur with test

--- specs/dinosaur_spec.js
+++ specs/dinosaur_spec.js
@@ -4,12 +4,16 @@ const Dinosaur = require('../dinosaur.js');
 describe('Dinosaur', function(){
 
   beforeEach(function(){
-    triceratops = new Dinosaur('Triceratops');
-  })
+    triceratops = new Dinosaur('Triceratops', 4);
+  });
 
   it('should have a type', function(){
     const actual = triceratops.type;
-    assert.equal(actual, 'Triceratops');
-  })
+    assert.strictEqual(actual, 'Triceratops');
+  });
+  it('should have a number of offspring per year', function(){
+    const actual = triceratops.offspringPerYear;
+    assert.strictEqual(actual, 4);
+  });
 
-})
+});
